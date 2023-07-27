abdb8e061047ca7b69b36185188dfd7404449917
Rohithumesh1 <118875932+Rohithumesh1@users.noreply.github.com>
2023-07-27 12:44:59 +0530

    Create PRIORITY Non pre-emptive.c

---
+++ PRIORITY Non pre-emptive.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+
+void priority_nonpreemptive(int processes[], int burst_time[], int priority[], int n) {
+    int waiting_time[n], turnaround_time[n], completion_time[n];
+    float total_waiting_time = 0, total_turnaround_time = 0;
+
+    // Sorting the processes based on priority in ascending order using selection sort
+    for (int i = 0; i < n; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (priority[j] < priority[min_idx])
+                min_idx = j;
+        }
+        // Swapping the processes, burst time, and priority
+        int temp = burst_time[i];
+        burst_time[i] = burst_time[min_idx];
+        burst_time[min_idx] = temp;
+
+        temp = priority[i];
+        priority[i] = priority[min_idx];
+        priority[min_idx] = temp;
+
+        temp = processes[i];
+        processes[i] = processes[min_idx];
+        processes[min_idx] = temp;
+    }
+
+  // Calculating completion time, waiting time, and turnaround time for each process
+    for (int i = 0; i < n; i++) {
+        if (i == 0)
+            completion_time[i] = burst_time[i];
+        else
+            completion_time[i] = completion_time[i - 1] + burst_time[i];
+
+        turnaround_time[i] = completion_time[i];
+        waiting_time[i] = turnaround_time[i] - burst_time[i];
+
+        total_waiting_time += waiting_time[i];
+        total_turnaround_time += turnaround_time[i];
+    }
+
+    printf("\nPriority (Non-preemptive) Scheduling Algorithm:\n");
+    printf("Process\tBurst Time\tPriority\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", processes[i], burst_time[i], priority[i], waiting_time[i], turnaround_time[i]);
+    }
+
+    printf("Average Waiting Time: %.2f\n", total_waiting_time / n);
+    printf("Average Turnaround Time: %.2f\n", total_turnaround_time / n);
+}
+int main() {
+    int n, choice;
+
+ printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    int processes[n];
+    int burst_time[n];
+    int priority[n];
+
+    printf("\nEnter the burst time and priority for each process:\n");
+    for (int i = 0; i < n; i++) {
+        processes[i]=i+1;
+        printf("Process %d\n", i + 1);
+        printf("Burst Time: ");
+        scanf("%d", &burst_time[i]);
+        printf("Priority: ");
+        scanf("%d", &priority[i]);
+        processes[i] = i + 1;
+    }
+     priority_nonpreemptive(processes, burst_time, priority, n);
+
+    return 0;
+}
